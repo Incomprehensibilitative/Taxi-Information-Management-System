14865d0407a9b804231edd8f8365d48eb1bd43a2
Incomprehensibilitative <dangmh.bi12-076@st.usth.edu.vn>
2023-03-31 16:08:45 +0700

    fix not looping id bug

--- Code Part/database_reader.py
+++ Code Part/database_reader.py
@@ -1,4 +1,6 @@
-from Domains import Customer, Vehicle, Driver, Invoice, Feedback
+"""Reader function
+Read all the data in a sheet and return it to an object list"""
+from Domains import Customer, Vehicle, Driver, Invoice
 import openpyxl
 
 # read database
@@ -8,7 +10,7 @@ data = openpyxl.load_workbook("Taxi-information.xlsx", read_only=True, data_only
 def take_customer_info():
     sheet = data['Customer']
     customer_list = []
-    for row in sheet:
+    for row in sheet.iter_rows(min_row=2):
         new_customer = Customer.Customer()
         new_customer.set_customer(row[0].value, row[1].value, row[2].value)
         customer_list.append(new_customer)
@@ -18,7 +20,7 @@ def take_customer_info():
 def take_vehicle_info():
     sheet = data['Vehicle']
     vehicle_list = []
-    for row in sheet:
+    for row in sheet.iter_rows(min_row=2):
         new_vehicle = Vehicle.Vehicle()
         new_vehicle.set_vehicle(row[0].value, row[1].value, row[2].value, row[3].value)
         vehicle_list.append(new_vehicle)
@@ -28,7 +30,7 @@ def take_vehicle_info():
 def take_driver_info():
     sheet = data['Driver']
     driver_list = []
-    for row in sheet:
+    for row in sheet.iter_rows(min_row=2):
         new_driver = Driver.Driver()
         new_driver.set_driver(row[0].value, row[1].value, row[2].value, row[3].value, row[4].value, row[5].value,
                               row[6].value)
@@ -39,19 +41,10 @@ def take_driver_info():
 def take_invoice_info():
     sheet = data['Invoice']
     invoice_list = []
-    for row in sheet:
+    for row in sheet.iter_rows(min_row=2):
         new_invoice = Invoice.Invoice()
         new_invoice.set_invoice(row[0].value, row[1].value, row[2].value, row[3].value, row[4].value, row[5].value,
                                 row[6].value, row[7].value)
         invoice_list.append(new_invoice)
     return invoice_list
 
-
-def take_feedback_info():
-    sheet = data['Feedback']
-    feedback_list = []
-    for row in sheet:
-        new_feedback = Feedback.Feedback()
-        new_feedback.set_feedback(row[0].value, row[1].value, row[2].value, row[3].value, row[4].value)
-        feedback_list.append(new_feedback)
-    return feedback_list
