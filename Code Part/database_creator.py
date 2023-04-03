084c4284fb90b859363371976f52f81c1c247662
Doan Dinh Dang <dangdoan2206@gmail.com>
2023-04-04 00:30:30 +0700

    remake random function

--- Code Part/database_creator.py
+++ Code Part/database_creator.py
@@ -15,6 +15,7 @@ def create_date():
     date = str(random_date)
     return date
 
+
 def create_price(vehicle_type):
     if vehicle_type == "5S":
         return "10000"
@@ -23,30 +24,94 @@ def create_price(vehicle_type):
     elif vehicle_type == "9S":
         return "15000"
 
+
 def create_customer_id(system):
-    customer_id = f"C{random.randint(0, 999)}"
+    # Creat customer id
+    sub_part = f"{random.randint(0, 999)}"
+    if len(sub_part) == 1:
+        sub_part = "00" + sub_part
+    elif len(sub_part) == 2:
+        sub_part = "0" + sub_part
+    customer_id = "C" + sub_part
+
+    # Check if the customer id is already existed
     customer_id_list = []
     for customer in system.get_list("customer"):
         customer_id_list.append(customer.get_id())
     while customer_id in customer_id_list:
-        customer_id = f"C{random.randint(0, 999)}"
+        sub_part = f"{random.randint(0, 999)}"
+        if len(sub_part) == 1:
+            sub_part = "00" + sub_part
+        elif len(sub_part) == 2:
+            sub_part = "0" + sub_part
+        customer_id = "C" + sub_part
     return customer_id
 
+
 def create_vehicle_id(system, type):
-    vehicle_id = f"{type}{random.randint(0,999)}"
+    # Create vehicle id
+    sub_part = f"{random.randint(0, 999)}"
+    if len(sub_part) == 1:
+        sub_part = "00" + sub_part
+    elif len(sub_part) == 2:
+        sub_part = "0" + sub_part
+    vehicle_id = type + sub_part
     vehicle_id_list = []
+
+    # Check if the vehicle id is already existed
     for vehicle in system.get_list("vehicle"):
         vehicle_id_list.append(vehicle.get_id())
     while vehicle_id in vehicle_id_list:
-        vehicle_id = f"{type}{random.randint(0, 999)}"
+        sub_part = f"{random.randint(0, 999)}"
+        if len(sub_part) == 1:
+            sub_part = "00" + sub_part
+        elif len(sub_part) == 2:
+            sub_part = "0" + sub_part
+        vehicle_id = type + sub_part
     return vehicle_id
 
+
 def create_driver_id(system):
-    driver_id = f"D{random.randint(0, 999)}"
-    driver_id_list = []
-    for driver in system.get_list("driver"):
-        driver_id_list.append(driver.get_id())
-    while driver_id in driver_id_list:
-        driver_id = f"D{random.randint(0, 999)}"
+    # Create driver id
+    sub_part = f"{random.randint(0, 999)}"
+    if len(sub_part) == 1:
+        sub_part = "00" + sub_part
+    elif len(sub_part) == 2:
+        sub_part = "0" + sub_part
+    driver_id = "D" + sub_part
+
+    # Check if the driver id is already existed
+    while driver_id in system.get_list("driver"):
+        sub_part = f"{random.randint(0, 999)}"
+        if len(sub_part) == 1:
+            sub_part = "00" + sub_part
+        elif len(sub_part) == 2:
+            sub_part = "0" + sub_part
+        driver_id = "D" + sub_part
     return driver_id
 
+
+def create_invoice_id(system):
+    # Create invoice id
+    sub_part = f"{random.randint(0, 999)}"
+    if len(sub_part) == 1:
+        sub_part = "00" + sub_part
+    elif len(sub_part) == 2:
+        sub_part = "0" + sub_part
+    invoice_id = "I" + sub_part
+
+    # Check if the invoice id is already existed
+    while invoice_id in system.get_list("invoice"):
+        sub_part = f"{random.randint(0, 999)}"
+        if len(sub_part) == 1:
+            sub_part = "00" + sub_part
+        elif len(sub_part) == 2:
+            sub_part = "0" + sub_part
+        invoice_id = "I" + sub_part
+    return invoice_id
+
+
+def create_phone_num():
+    sub_part = random.randint(10000000, 99999999)
+    phone_num = "09" + str(sub_part)
+    return phone_num
