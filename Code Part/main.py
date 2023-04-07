1feb545a406f7deb3bd40b4cdecfa794cf80a4a2
thdg <92166170+thdgg@users.noreply.github.com>
2023-04-07 17:33:07 +0700

    UserLogin -> pyQT, fix main and resize + fix stuff in AdminGUI

--- Code Part/main.py
+++ Code Part/main.py
@@ -1,8 +1,6 @@
 import UserLoginGUI
-import AdminGUI
 import Management
 
 if __name__ == "__main__":
     system = Management.Management()
-    # AdminGUI.main(system) # For running disable this line
-    UserLoginGUI.user_login(system) # when debugging disable this line
+    UserLoginGUI.user_login(system)
